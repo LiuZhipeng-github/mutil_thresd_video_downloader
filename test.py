8d2e9a6002408a2e7e2b7ebd3d4f5504998c98bd
刘志鹏 <liuzhipeng@MacBook-Pro.local>
2020-10-15 19:30:49 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+'''
+https://m3u8.pps11.com/wodeshipin_water_m3u8/zipaitoupai/80_20200110170128800/0220.ts
+'''
+
+import datetime
+# import time
+# name = str(time.strftime("%H-%M-%S"))
+# print(name)
+
+# now = time.time()  # 当前时间 float类型
+# n =time.strftime("%m-%d %M-%S")
+# print(now)
+# URL= 'https://m3u8.pps11.com/wodeshipin_water_m3u8/zipaitoupai/80_20200110170128800/0220.ts'
+# n = URL.rsplit('/',1)
+#
+# # n = int(n.split('.')[0])
+# print(n)
+def pp(i):
+    print(i)
+    for j in range(i,3):
+        pp(j+1)
+pp(0)
